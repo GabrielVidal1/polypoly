9296399d5e3d74ef648c09709c5ac0221a7703d4
Gabriel Vidal-Ayrinhac <gabriel.vidal-ayrinhac@epita.fr>
2020-11-15 00:50:36 +0100

    add project prototype

---
+++ PolyPoly/Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    const int MINIMUM_PLAYER_NUMBER = 1;
+    
+    public List<Player> players;
+    public Player LocalPlayer { get; private set; }
+
+    public void AddPlayer(Player player)
+    {
+        players.Add(player);
+        LocalPlayer = player;
+        if (players.Count >= MINIMUM_PLAYER_NUMBER)
+            StartGame();
+    }
+
+    void StartGame()
+    {
+        
+    }
+}
